589fe8852081094ad5ee3b63da5e7dfaf154461b
iwanhermawan29 <iwan.hermawan@proenergi.co.id>
2025-12-02 15:40:53 +0700

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight flex items-center gap-2">
-            👥 Dashboard Tim IT
+            👥 Dashboard Tim IT Departemen
         </h2>
     </x-slot>
 
